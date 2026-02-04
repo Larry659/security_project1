62b92e93178ae66f7bfd0e8da111df1d756b8bed
lanre <larryfunmbi26@gmail.com>
2026-02-04 16:49:10 +0100

    NoOpPasswordEncoder

---
+++ src/main/java/com/lanre/security_project1/configuration/ProjectConfig.java
@@ -0,0 +1,33 @@
+package com.lanre.security_project1.configuration;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+@Configuration
+public class ProjectConfig {
+    @Bean
+    UserDetailsService userDetailsService() {
+
+        var user = User.withUsername("john")
+                .password("12345")
+                .authorities("read")
+                .build();
+
+        var user2 = User.withUsername("lanre")
+                .password("54321")
+                .authorities("read", "write")
+                .build();
+        return new InMemoryUserDetailsManager(user, user2);
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return NoOpPasswordEncoder.getInstance();
+    }
+}
