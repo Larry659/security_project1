24500928b9c45cc89c81b5cc10b445aaade2831a
lanre <larryfunmbi26@gmail.com>
2026-02-04 09:50:09 +0100

    first commit

---
+++ src/main/java/com/lanre/security_project1/SecurityProject1Application.java
@@ -0,0 +1,13 @@
+package com.lanre.security_project1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecurityProject1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecurityProject1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/lanre/security_project1/SecurityProject1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lanre.security_project1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurityProject1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
