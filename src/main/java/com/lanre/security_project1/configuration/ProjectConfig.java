0b4781c5708d20fd06ef6dad754e485cb317d391
lanre <larryfunmbi26@gmail.com>
2026-02-04 17:23:24 +0100

    endpoint authorization

--- src/main/java/com/lanre/security_project1/configuration/ProjectConfig.java
+++ src/main/java/com/lanre/security_project1/configuration/ProjectConfig.java
@@ -3,11 +3,14 @@ package com.lanre.security_project1.configuration;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class ProjectConfig {
@@ -30,4 +33,18 @@ public class ProjectConfig {
     public PasswordEncoder passwordEncoder() {
         return NoOpPasswordEncoder.getInstance();
     }
+//To customize the handling of authentication and authorization, we’ll need to define a
+//bean of type SecurityFilterChain
+    @Bean
+    SecurityFilterChain configure(HttpSecurity http)
+            throws Exception {
+       http.httpBasic(Customizer.withDefaults()); //App uses HTTP Basic authentication.
+//        http.authorizeHttpRequests(
+//                c -> c.anyRequest().authenticated() //All the requests require  authentication i.e must be checked.
+        http.authorizeHttpRequests(
+                c -> c.anyRequest().permitAll() //None of the requests need  to be authenticated
+        );
+        return http.build();
+    }
+
 }
