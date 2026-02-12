67e9418381a7ccba16a7eb0c5c709070b23b10f3
lanre <larryfunmbi26@gmail.com>
2026-02-12 14:22:06 +0100

    jdbcUserDetailsManager

--- src/main/java/com/lanre/security_project1/configuration/UserManagementConfig.java
+++ src/main/java/com/lanre/security_project1/configuration/UserManagementConfig.java
@@ -11,7 +11,9 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
 
+import javax.sql.DataSource;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -31,14 +33,27 @@ public class UserManagementConfig {
 //                .build();
 //        return new InMemoryUserDetailsManager(user, user2);
 //    }
-@Bean
-public UserDetailsService userDetailsService() {
-    List<SimpleGrantedAuthority> authorities =
-            List.of(new SimpleGrantedAuthority("read"));
-    UserDetails u = new User("john", "12345",authorities);
-    List<UserDetails> users = List.of(u);
-    return new InMemoryUserDetailsService(users);
-}
+//@Bean
+//public UserDetailsService userDetailsService() {
+//    List<SimpleGrantedAuthority> authorities =
+//            List.of(new SimpleGrantedAuthority("read"));
+//    UserDetails u = new User("john", "12345",authorities);
+//    List<UserDetails> users = List.of(u);
+//    return new InMemoryUserDetailsService(users);
+
+//using datasource
+    @Bean
+    public UserDetailsService userDetailsService(DataSource dataSource) {
+       // return new JdbcUserDetailsManager(dataSource);
+        String usersByUsernameQuery =
+                "select username, password, enabled from users where username = ?";
+        String authsByUserQuery =
+                "select username, authority from spring.authorities where username = ?";
+        var userDetailsManager = new JdbcUserDetailsManager(dataSource);
+        userDetailsManager.setUsersByUsernameQuery(usersByUsernameQuery);
+        userDetailsManager.setAuthoritiesByUsernameQuery(authsByUserQuery);
+        return userDetailsManager;
+    }
 
     @Bean
     public PasswordEncoder passwordEncoder() {
@@ -46,3 +61,4 @@ public UserDetailsService userDetailsService() {
     }
 
 }
+
